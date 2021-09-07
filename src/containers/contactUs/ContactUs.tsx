ff3d5a966feea55a08f39718f768b034663500c4
Luccin Masirika <luccinmasirika@gmail.com>
2021-09-07 17:15:58 +0100

    test

--- src/containers/contactUs/ContactUs.tsx
+++ src/containers/contactUs/ContactUs.tsx
@@ -9,12 +9,14 @@ export const ContactUs: React.FC<IContactUsProps> = (props) => {
   return (
     <>
       <Banner state={false} title='Contact Us' />
-      <div className='container w-full flex flex-wrap p-4 md:px-32 h-auto bg-white md:flex-nowrap relative z-10 py-8  mx-auto'>
-        <div className='w-full md:w-2/4 md:p-12 p-2'>
-          <img src={Image} alt='Contact' />
-        </div>
-        <div className='w-full md:w-2/4'>
-          <ContactForm />
+      <div className='w-full bg-white relative z-10'>
+        <div className='container w-full flex flex-wrap p-4 md:px-32 h-auto  md:flex-nowrap  py-8  mx-auto'>
+          <div className='w-full md:w-2/4 md:p-12 p-2'>
+            <img src={Image} alt='Contact' />
+          </div>
+          <div className='w-full md:w-2/4'>
+            <ContactForm />
+          </div>
         </div>
       </div>
     </>
